85f6d3b1ac0f8422646e8a3bacaa8f4727742f52
scrubi19 <scrubi.memo@yandex.ru>
2020-09-29 23:43:16 +0700

    fix conf.py

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,3 @@
-import os
-import sys
-
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
@@ -54,22 +51,6 @@ pygments_style = None
 
 numpydoc_show_class_members = False
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
@@ -108,36 +89,6 @@ latex_elements = {
     #
     # 'figure_align': 'htbp',
 }
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'My_Docs.tex', 'My\\_Docs Documentation',
-     'Scrubi19', 'manual'),
-]
-
-
-# -- Options for manual page output ------------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'my_docs', 'My_Docs Documentation',
-     [author], 1)
-]
-
-
-# -- Options for Texinfo output ----------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'My_Docs', 'My_Docs Documentation',
-     author, 'My_Docs', 'One line description of project.',
-     'Miscellaneous'),
-]
 
 
 # -- Options for Epub output -------------------------------------------------
