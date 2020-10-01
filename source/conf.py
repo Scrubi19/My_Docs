a5a7bcaedbdf997dcc008b960df68c251a651d19
scrubi19 <scrubi.memo@yandex.ru>
2020-10-01 19:17:42 +0700

    update color's

--- source/conf.py
+++ source/conf.py
@@ -1,6 +1,6 @@
+import os
+import sys
 import sphinx_rtd_theme
-
-# -- Project information -----------------------------------------------------
 
 project = 'My_Docs'
 copyright = '2020, Bulatov Alexandr'
@@ -11,25 +11,8 @@ version = ''
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
 
-
-# -- General configuration ---------------------------------------------------
-
-# If your documentation needs a minimal Sphinx version, state it here.
-#
 # needs_sphinx = '1.0'
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
-]
-
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 html_static_path = ['_static']
 
@@ -41,36 +24,16 @@ master_doc = 'index'
 
 language = 'ru'
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['cryptography.rst', 'ruby.rst', 'sql.rst']
 
-# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
 
 numpydoc_show_class_members = False
 
 html_static_path = ['_static']
 
-# Custom sidebar templates, must be a dictionary that maps document names
-# to template names.
-#
-# The default sidebars (for documents that don't match any pattern) are
-# defined by theme itself.  Builtin themes are using these templates by
-# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
-# 'searchbox.html']``.
-#
-# html_sidebars = {}
-
-
-# -- Options for HTMLHelp output ---------------------------------------------
-
-# Output file base name for HTML help builder.
 htmlhelp_basename = 'My_Docsdoc'
 
-
-# -- Options for LaTeX output ------------------------------------------------
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
@@ -87,38 +50,26 @@ latex_elements = {
 
     # Latex figure (float) alignment
     #
-    # 'figure_align': 'htbp',
+    # 'figure_align': 'flushleft'
 }
 
-
-# -- Options for Epub output -------------------------------------------------
-
-# Bibliographic Dublin Core info.
 epub_title = project
 
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#
-# epub_identifier = ''
-
-# A unique identification for the text.
-#
-# epub_uid = ''
-
-# A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
-
 
 html_theme_path = ['themes']  # папка внутри source, где лежат папки тем
 
+
+# sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
+
 html_theme_options = {
     'canonical_url': '',
     'logo_only': True,
     'display_version': False,
     'prev_next_buttons_location': '',
     'style_external_links': True,
-    'style_nav_header_background': '#FFB23D',
+    'style_nav_header_background': '#343131',
     # Toc options
     'collapse_navigation': False,
     'sticky_navigation': True,
@@ -126,3 +77,11 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False
 }
+
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    'sphinx_rtd_theme'
+]
